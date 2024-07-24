e749289cee285e20221825e3afd6c2aa79d8b8da
Kasim Ibrahim <quasim@Kasims-MacBook-Air.local>
2024-07-24 17:31:52 +0000

    MODIFICATION : added all country images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from countries import Countries
+from PIL import Image, ImageTk
 from controls import Controls
 import random as r
 
@@ -9,16 +10,17 @@ window.minsize(500, 500)
 
 Label(window, text='WELCOME TO KNOW YOUR COUNTRIES').pack()
 
-img = PhotoImage(file='titleicon.png')
+file = Image.open('./titleicon.png')
+img = ImageTk.PhotoImage(file)
 img_frame = Label(window, image=img)
+img_frame.image = img
 img_frame.pack()
 score = 0
 
 game = Countries(window)
 
-if score <= 50:
-    bucketList = game.africa
-elif score > 50 and score <= 100:
+bucketList = game.africa
+if score > 50 and score <= 100:
     bucketList = game.asia
 elif score > 100 and score <= 120:
     bucketList = game.north_america
@@ -38,15 +40,19 @@ else:
 
 
 def shuffle():
+    global img
     selected = r.choice(list(bucketList.keys()))
     current_question = bucketList[selected]
+    img = PhotoImage(current_question.location)
+    img_frame.config(image=img)
+    img_frame.pack()
     current_question.show()
-    img_frame.config(image=PhotoImage(current_question.location))
+    print(current_question.location)
 
 
-nxt = Button(window, text="NEXT", command=shuffle)
-nxt.pack()
-ext = Button(window, text='EXIT', command=window.quit)
-ext.pack()
+next = Button(window, text="NEXT", command=shuffle)
+next.pack()
+exit = Button(window, text='EXIT', command=exit)
+exit.pack(side=BOTTOM)
 
 window.mainloop()
