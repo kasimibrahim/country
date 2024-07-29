63afc647158407f78c17dfde065f71a56c90117b
Kasim Ibrahim <quasim@Kasims-MacBook-Air.local>
2024-07-29 17:27:39 +0000

    MODIFICATION : added all country images

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from countries import Countries
-from PIL import Image, ImageTk
 from controls import Controls
 import random as r
 
@@ -10,16 +9,17 @@ window.minsize(500, 500)
 
 Label(window, text='WELCOME TO KNOW YOUR COUNTRIES').pack()
 
-file = Image.open('./titleicon.png')
-img = ImageTk.PhotoImage(file)
-img_frame = Label(window, image=img)
-img_frame.image = img
+file = PhotoImage(file='./titleicon.png')
+img_frame = Label(window, image=file)
 img_frame.pack()
 score = 0
 
 game = Countries(window)
+right = False
 
 bucketList = game.africa
+question_tracker = Countries(window).CountryQuestion('', window)
+
 if score > 50 and score <= 100:
     bucketList = game.asia
 elif score > 100 and score <= 120:
@@ -40,19 +40,33 @@ else:
 
 
 def shuffle():
-    global img
+    global question_tracker
     selected = r.choice(list(bucketList.keys()))
     current_question = bucketList[selected]
     img = PhotoImage(current_question.location)
     img_frame.config(image=img)
-    img_frame.pack()
     current_question.show()
+    question_tracker = current_question
     print(current_question.location)
 
 
-next = Button(window, text="NEXT", command=shuffle)
-next.pack()
-exit = Button(window, text='EXIT', command=exit)
-exit.pack(side=BOTTOM)
+def submit():
+    global right
+    val = question_tracker.get_answer()
+    print('answered:', val)
+    print(game.shorthand[question_tracker.key])
+    right = val == game.shorthand[question_tracker.key]
+
+
+nxt = Button(window, text="NEXT", command=shuffle)
+submit = Button(window, text='SUBMIT', command=submit)
+nxt.pack()
+submit.pack()
+Label(window, text='CURRENT SCORE: '+str(score)).pack()
+ext = Button(window, text='EXIT', command=exit)
+ext.pack(side=BOTTOM)
+
+if right:
+    score += 50
 
 window.mainloop()
